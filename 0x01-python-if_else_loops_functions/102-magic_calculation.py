0f9972a98fabfe732f9e4e6a0ac9e2e17d013f41
bereket <bereketgebeyehu77@gmail.com>
2022-05-31 15:07:04 -0700

    Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    if n >= 0:
-        newstr = str[:n] + str[n + 1:]
-        return (newstr)
+def magic_calculation(a, b, c):
+    if a < b:
+        return(c)
+    elif c > b:
+        return(a + b)
     else:
-        return (str)
+        return(a * b - c)
