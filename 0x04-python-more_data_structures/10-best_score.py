69babccfcdffa67655f68299ffea6a86ce5a8669
bereket <bereketgebeyehu77@gmail.com>
2022-06-09 08:34:05 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(my_dict):
     if my_dict and len(my_dict):
         max = list(my_dict.keys())[0]
-        for key in my_dict
+        for key in my_dict:
             if my_dict[key] > my_dict[max]:
                 max = key
         return max
