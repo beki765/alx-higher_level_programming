2febe7942267afcd0c2a3e2bcbdc6abf99c3eda1
bereket <bereketgebeyehu77@gmail.com>
2022-06-21 16:05:49 -0700

    Tak 6

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,10 +39,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        
-
-        """Prints in stdout the square with the character #:"""
-
+    """Prints in stdout the square with the character """
         if self.__size == 0:
             print("")
         else:
