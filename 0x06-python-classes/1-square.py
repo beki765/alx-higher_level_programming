e752303177649388c0a94b6526c93d6e57c0eaab
bereket <bereketgebeyehu77@gmail.com>
2022-06-21 10:23:00 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,15 +5,15 @@
 class Square:
     """Represents a square
 
-Attributes:
-__size (int): size of a side of the square
-"""
-def __init__(self, size):
-"""Initializes a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
 
-Args:
-    size (int): size of a side of the square
+        Args:
+            size (int): size of a side of the square
 
-Returns: None
-"""
-self.__size = size
+        Returns: None
+        """
+        self.__size = size
