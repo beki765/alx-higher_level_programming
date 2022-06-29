cb4200f522d3b445b9bec6f5528f2525cea60fa0
bereket <bereketgebeyehu77@gmail.com>
2022-06-29 08:04:23 -0700

    Task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -40,14 +40,20 @@ class Rectangle:
         if value < 0:
             raise TypeError("height must be >= 0")
         self.__height = value
-                                                                                    def area(self):
+
+    def area(self):
         """Returns the current rectangle area"""
-        return self.__width * self.__height                                                                                                                         def perimeter(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
         """Return the current rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width + self.__height) * 2                                                                                                                   def __str__(self):
-        """Return a string with rectangle to stdout"""                                 if self.__height == 0 or self.__width == 0:
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Return a string with rectangle to stdout"""
+        if self.__height == 0 or self.__width == 0:
             return ''
         _str = ""
         for i in range(self.__height):
@@ -55,15 +61,21 @@ class Rectangle:
             if self.height != i + 1:
                 _str += '\n'
         return _str
-                                                                                    def __repr__(self):
+
+    def __repr__(self):
         """Return a string representation of the rectangle"""
         return("Rectangle({}, {})".format(self.width, self.height))
-                                                                                    def __del__(self):
+
+    def __del__(self):
         """Prints the message"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1                                                                                                                          @staticmethod
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Returns the biggest rectangle based on the area"""                           if not isinstance(rect_11, Rectangle):                                              raise TypeError("rect_1 must be an instance of Rectangle")
+        """Returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
