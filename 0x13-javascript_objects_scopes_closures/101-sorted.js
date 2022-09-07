b93eb88f7fde84f76d970ac9ff3c9c5cf4c628ea
beki765 <bereketgebeyehu77@gmail.com>
2022-09-07 21:07:06 +0000

    Advance task 2b

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node
+#!/user/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
+let  newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
